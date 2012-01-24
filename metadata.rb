38726d0638ccdc9e345a3043a723c08e65cfd49b
philcryer <phil@cryer.us>
2012-01-24 12:53:46 -0600

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Woods Hole Marine Biological Laboratory"
+maintainer_email "pcryer@mbl.edu"
+license          "Apache 2.0"
+description      "Installs/Configures bsdutils"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
